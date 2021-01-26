e0b2e27c1d9771367df96cbf5249bddaaf4cc861
Crystal McLaughlin <c.mclaughlin1@yahoo.com>
2021-01-26 15:38:24 -0500

    movie 2 updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("yo");
         System.out.println("lets see what happens");
+        System.out.println("this movie is good");
     }
 }
