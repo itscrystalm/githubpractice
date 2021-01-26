830a263ec738e1043a05ddd68d4c44df89b09060
Crystal McLaughlin <c.mclaughlin1@yahoo.com>
2021-01-26 15:25:04 -0500

    movie 1 updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("yo");
         System.out.println("Hello world b21");
+        System.out.println("idk whats going on");
+        System.out.println("i am confused");
     }
 }
